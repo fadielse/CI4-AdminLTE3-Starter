b0543dac61a87994d03a47fc03e7475f919e334e
Fadilah Hasan <fadielse@gmail.com>
2020-10-12 19:40:58 +0700

    config base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/YOUR_PROJECT_NAME/';
+	public $baseURL = 'http://localhost/AdminLteStarter/';
 
 	/*
 	|--------------------------------------------------------------------------
